afb0228073189ed0cba468c2cb220a7bb26704bf
bilalkeremoglu <bilalkeremoglu@sakarya.edu.tr>
2024-06-26 16:50:49 +0300

    WebApp

---
+++ BA/BA.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BA.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
